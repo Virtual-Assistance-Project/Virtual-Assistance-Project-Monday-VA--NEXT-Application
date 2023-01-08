d3019eca4eccb66abfd86a2ec704f4a63b760f39
Guilherme Bernardo <guilhermebernardo@id.uff.br>
2023-01-07 23:22:51 -0300

    FEAT: login html done

--- virtual-assistence-monday-va-next-application/src/components/Login/index.tsx
+++ virtual-assistence-monday-va-next-application/src/components/Login/index.tsx
@@ -1,12 +1,57 @@
 import { Container } from "./style"
+import MondayIcon from "../../public/Icon/MondayIcon.svg"
+import { useForm } from 'react-hook-form';
+import { ILoginUser } from "../../interfaces";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { schema } from "../../validators/loginUser";
 
-
+const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILoginUser>({ resolver: yupResolver(schema) });
 
 
 export const Component = () => {
     return <Container>
-        <h1>
-            testando
-        </h1>
+        <main>
+            <img src={MondayIcon} alt="Logo" />
+            <div>
+                <h1>
+                    Monday
+                </h1>
+                <h2>
+                    Conecte e controle sua vida <br/>com seu mobile
+                </h2>
+                <div>
+                    <form onSubmit={handleSubmit(()=>console.log("Função de envio para api aqui."))}>
+                        
+                        <input 
+                            placeholder="Usuário..." 
+                            type='text' 
+                            id='username' 
+                            {...register('username')} 
+                        />
+                        <p>{errors.username?.message}</p>
+
+                        <input 
+                            placeholder="Senha..." 
+                            type='password' 
+                            id='password' 
+                            {...register('password')} 
+                        />
+                        <p>{errors.password?.message}</p>
+
+                        <button>
+                            Entrar
+                        </button>
+
+                    </form>
+                    <a href="/register">
+                        Cadastre-se aqui...
+                    </a>
+                </div>
+            </div>
+        </main>
     </Container>
 }
--- virtual-assistence-monday-va-next-application/src/interfaces/index.ts
+++ virtual-assistence-monday-va-next-application/src/interfaces/index.ts
@@ -2,3 +2,8 @@ export interface ILink {
     link:string
     content:string
 }
+
+export interface ILoginUser {
+    username:string
+    password:string
+}
