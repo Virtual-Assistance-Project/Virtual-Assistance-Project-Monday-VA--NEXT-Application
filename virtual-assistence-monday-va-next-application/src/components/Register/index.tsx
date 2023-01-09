9859718b6de3e07cf77257d4c0ceb6daac415cf9
Guilherme Bernardo <guilhermebernardo@id.uff.br>
2023-01-09 01:14:51 -0300

    FEAT: Register logic progress

--- virtual-assistence-monday-va-next-application/src/components/Register/index.tsx
+++ virtual-assistence-monday-va-next-application/src/components/Register/index.tsx
@@ -1,45 +1,51 @@
+import React from "react";
+import questions from "./questions";
 
 
 export const Component = () => {
-
-    return(
-        <></>
-    )
+  
+    function ChatForm() {
+        const [step, setStep] = React.useState(0);
+        const [formData, setFormData] = React.useState({});
+    
+        const handleChange = (event:any) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+        });
+        };
+    
+        const handleSubmit = (event:any) => {
+        event.preventDefault();
+        setStep(step + 1);
+        };
+        const renderStep = () => {
+            const currentQuestion = questions[step];
+            return (
+            <div>
+                <p>{currentQuestion.question}</p>
+                <input
+                type={currentQuestion.inputType}
+                name={currentQuestion.id}
+                onChange={handleChange}
+                />
+                <button type="submit">Next</button>
+            </div>
+            );
+        };
+        
+        return (
+            <form onSubmit={handleSubmit}>
+            {step < questions.length ? renderStep() : <p>Obrigado por se registrar!</p>}
+            </form>
+        );
+        }
+        
+        export default ChatForm;
+        
 }
 
-// const questions = [
-//     {
-//       id: 1,
-//       question: "What is your name?",
-//       inputType: "text",
-//     },
-//     {
-//       id: 2,
-//       question: "What is your email?",
-//       inputType: "email",
-//     },
-//     {
-//       id: 3,
-//       question: "What is your phone number?",
-//       inputType: "tel",
-//     },
-//   ];
-  
-//   function ChatForm() {
-//     const [step, setStep] = React.useState(0);
-//     const [formData, setFormData] = React.useState({});
-  
-//     const handleChange = (event) => {
-//       setFormData({
-//         ...formData,
-//         [event.target.name]: event.target.value,
-//       });
-//     };
-  
-//     const handleSubmit = (event) => {
-//       event.preventDefault();
-//       setStep(step + 1);
-//     };
+
   
 //     const renderStep = () => {
 //       const currentQuestion = questions[step];
